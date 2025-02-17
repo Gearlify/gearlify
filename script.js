aabbbf6bc02d08cbb1050a1aca4ee2c3190ab194
Gearlify <gearlifycorporation@gmail.com>
2025-02-16 22:30:05 -0800

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const typingDuration = 8500;
 // Check if the effect has already played
 if (sessionStorage.getItem("typewriterPlayed")) {
   // Redirect immediately if the effect has played
-  window.location.href = ".../login-form-with-database-connection-main/index.php"; // Replace with your main page link
+  window.location.href = ".../login-form-with-database-connection-main/indexx.php"; // Replace with your main page link
 } else {
   // Set session storage to remember the effect played
   sessionStorage.setItem("typewriterPlayed", "true");
