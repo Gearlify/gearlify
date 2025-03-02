fc313a44bf1a7ffd894b83403148265b365fcf1e
Gearlify <gearlifycorporation@gmail.com>
2025-03-02 12:34:10 -0800

    script.js

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const typingDuration = 8500;
 // Check if the effect has already played
 if (sessionStorage.getItem("typewriterPlayed")) {
   // Redirect immediately if the effect has played
-  window.location.href = "http://ayotheg.42web.io/?i=1"; // Replace with your main page link
+  window.location.href = "index.html"; // Replace with your main page link
 } else {
   // Set session storage to remember the effect played
   sessionStorage.setItem("typewriterPlayed", "true");
 
   // Redirect after the animation ends
   setTimeout(() => {
-    window.location.href = "http://ayotheg.42web.io/?i=1"; // Replace with your main page link
+    window.location.href = "index.html"; // Replace with your main page link
   }, typingDuration);
 }
 
